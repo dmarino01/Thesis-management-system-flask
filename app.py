425cfe49d219812d90d7a203844be221a738cbed
Diego Alejandro Marino Ramos <melkor91>
2023-09-09 20:59:55 -0500

    Plantilla Base + Views

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("index.html")
 
-@app.route("/login", methods=["POST", "GET"])
+
+@app.route('/')
+@app.route('/index')
+def home():
+    return render_template("home.html")
+
+@app.route("/login")
 def login():
     return render_template("login.html")
 
@@ -14,5 +17,24 @@ def login():
 def register():
     return render_template("register.html")
 
+@app.route('/usuarios')
+def usuarios():
+    # Handle the usuarios page logic here
+    return render_template('usuarios.html')
+
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
+@app.route('/temas')
+def temas():
+    # Handle the temas page logic here
+    return render_template('temas.html')
+
+@app.route('/libreria')
+def libreria():
+    # Handle the librery page logic here
+    return render_template('libreria.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
