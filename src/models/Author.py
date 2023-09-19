57b7fc0e228c1699d37a4dfaee3ae541a2009d5c
DiegoMarinoR <dmarino53@outlook.com>
2023-09-19 15:47:02 -0500

    Libreria view

--- src/models/Author.py
+++ src/models/Author.py
@@ -1,5 +1,10 @@
 class Author():
 
+    def __init__(self, student_code, author_id, person_id):
+        self.student_code = student_code
+        self.author_id = author_id
+        self.person_id = person_id
+
     def __init__(self, student_code, author_id, person_id, firstname, lastname, phone, address, email, is_deleted=False):
         self.student_code = student_code
         self.author_id = author_id
