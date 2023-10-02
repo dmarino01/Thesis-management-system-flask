cef80023475479a1350334663b71778b110638fb
DiegoMarinoR <dmarino53@outlook.com>
2023-10-02 14:09:34 -0500

    Routes and Blueprints re-arrenged

--- src/blueprints/user_blueprint.py
+++ src/blueprints/user_blueprint.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from controllers.ControllerUser import ControllerUser
 from models.User import User
-from flask_login import login_required
 
 from config import db
 
