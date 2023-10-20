c36b349fbb7006cfa0dfa652060376f7f479fbda
dmarino01 <dmarino53@outlook.com>
2023-10-20 11:10:43 -0500

    Grade saved, to just show it to student

--- src/blueprints/review_blueprint.py
+++ src/blueprints/review_blueprint.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from controllers.ControllerThesis import ControllerThesis
 from controllers.ControllerReview import ControllerReview
-from flask_login import login_required
+from flask_login import current_user, login_required
+import datetime
 
 from config import db
 
@@ -18,3 +19,16 @@ def review_thesis(id):
 def review_thesis_page(id):
     data = ControllerThesis.get_thesis_by_id(db, id)
     return render_template('review/review.html', thesis=data)
+
+@review_bp.route('/save_review/<int:id>', methods=['GET', 'POST'])
+@login_required
+def save_review(id):
+    try:
+        person_id = current_user.person_id
+        nota = request.form['nota']
+        comentario = request.form['comentario']
+        date = datetime.datetime.now()
+        ControllerReview.createReview(db, nota, comentario, id, person_id, date)
+        return redirect(url_for('review.review_thesis', id=person_id))
+    except Exception as ex:
+        raise Exception(ex)
--- src/controllers/ControllerReview.py
+++ src/controllers/ControllerReview.py
@@ -25,8 +25,24 @@ class ControllerReview():
             raise Exception(ex)
 
     @classmethod
-    def save_review(cls, db):
+    def createReview(cls, db, nota, comentario, id, person_id, date):
         try:
+            session = db.session()
+            sql = text(
+            "SET @reviewer_id = (select reviewer_id from reviewer where person_id = :person_id); "    
+            "INSERT INTO REVIEW (thesis_id, review_date, rating, reviewer_id, comment) "
+            "VALUES (:thesis_id, :date, :nota, @reviewer_id, :comment); "
+            "UPDATE thesis SET thesis_status_id = '2' WHERE thesis_id = :thesis_id; "
+            )
+            params = {
+                'nota' : nota,
+                'comment' : comentario,
+                'thesis_id' : id,
+                'person_id' : person_id,
+                'date' : date
+            }
+            session.execute(sql, params)
+            session.commit()
             return {'message': 'Review created successfully'}, 201
         except Exception as ex:
             raise Exception(ex)
