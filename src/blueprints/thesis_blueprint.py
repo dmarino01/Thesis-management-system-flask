461f19a81effb8ea34dc72f08839e4906e595da0
dmarino01 <dmarino53@outlook.com>
2023-12-30 13:32:53 -0500

    Register, View Thesis Project Updated

--- src/blueprints/thesis_blueprint.py
+++ src/blueprints/thesis_blueprint.py
@@ -3,91 +3,103 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required
 from controllers.ControllerThesis import ControllerThesis
 from controllers.ControllerRecommendation import ControllerRecommendation
-from werkzeug.utils import secure_filename 
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
 from config import db
 
-thesis_bp = Blueprint('thesis', __name__)
+thesis_bp = Blueprint("thesis", __name__)
 UPLOAD_FOLDER = os.path.join("src", "static", "file", "thesis")
 
-#Thesis Index
-@thesis_bp.route('/myThesis')
+
+# Thesis Index
+@thesis_bp.route("/myThesis")
 @login_required
 def myThesis():
     data = ControllerThesis.getThesis(db)
     return render_template("myThesis/index.html", thesis=data)
 
-#View Thesis Page
-@thesis_bp.route('/view_thesis_page/<int:id>', methods=['GET'])
+
+# View Thesis Page
+@thesis_bp.route("/view_thesis_page/<int:id>", methods=["GET"])
 @login_required
 def view_thesis_page(id):
-    thesis = ControllerThesis.get_thesis_by_id(db, id)
-    recommendations = ControllerRecommendation.get_recommendations_by_thesis_id(db, id)
-    return render_template("myThesis/detail.html", thesis=thesis, recommendations = recommendations)
+    try:
+        thesis = ControllerThesis.get_thesis_by_id(db, id)
+        recommendations = ControllerRecommendation.get_recommendations_by_thesis_id(db, id)
+        return render_template("myThesis/detail.html", thesis=thesis, recommendations=recommendations)
+    except Exception as ex:   
+        print(f"Error: {ex}")
+        raise Exception(ex)
 
-#Edit Thesis Form
-@thesis_bp.route('/edit_thesis_form/<int:id>', methods=['GET'])
+# Edit Thesis Form
+@thesis_bp.route("/edit_thesis_form/<int:id>", methods=["GET"])
 @login_required
 def edit_thesis_form(id):
     thesis = ControllerThesis.get_thesis_by_id(db, id)
+
     return render_template("myThesis/edit.html", thesis=thesis)
 
-#Thesis Create Form
-@thesis_bp.route('/create_thesis_form')
+
+# Thesis Create Form
+@thesis_bp.route("/create_thesis_form")
 @login_required
 def create_thesis_form():
     return render_template("myThesis/create.html")
 
-#Update Thesis Route
-@thesis_bp.route('/update_thesis/<int:id>', methods=['POST'])
+
+# Update Thesis Route
+@thesis_bp.route("/update_thesis/<int:id>", methods=["POST"])
 @login_required
 def update_thesis(id):
     try:
-        title = request.form['title']
-        abstract = request.form['abstract']
+        title = request.form["title"]
+        abstract = request.form["abstract"]
 
-        if (title != '' and abstract != ''):
+        if title != "" and abstract != "":
             ControllerThesis.updateThesis(db, id, title, abstract)
-            return redirect(url_for('thesis.edit_thesis_form', id=id))
+            return redirect(url_for("thesis.edit_thesis_form", id=id))
         else:
             flash("No deben haber campos vacios...")
-            return redirect(url_for('thesis.edit_thesis_form', id=id))
+            return redirect(url_for("thesis.edit_thesis_form", id=id))
     except Exception as ex:
         flash(f"Error updating thesis: {str(ex)}")
-        return redirect(url_for('thesis.edit_thesis_form', id=id))
+        return redirect(url_for("thesis.edit_thesis_form", id=id))
 
-#Save Thesis Route
-@thesis_bp.route('/save_thesis', methods=['POST'])
+
+# Save Thesis Project Route
+@thesis_bp.route("/save_thesis", methods=["POST"])
 @login_required
 def save_thesis():
     try:
-        title = request.form['title']
-        abstract = request.form['abstract']
-        pdf_file = request.files['pdf_file']
+        title = request.form["title"]
+        abstract = request.form["abstract"]
+        pdf_file = request.files["pdf_file"]
         if title and abstract and pdf_file:
             os.makedirs(UPLOAD_FOLDER, exist_ok=True)
             if pdf_file and allowed_file(pdf_file.filename):
                 filename = secure_filename(pdf_file.filename)
                 pdf_path = os.path.join(UPLOAD_FOLDER, filename)
                 pdf_file.save(pdf_path)
-                ControllerThesis.createThesis(db, title, abstract, filename)
-                return redirect(url_for('thesis.myThesis'))
+                ControllerThesis.createProjectThesis(db, title, abstract, filename)
+                return redirect(url_for("thesis.myThesis"))
             else:
                 flash("Invalid file format. Please upload a PDF file.")
-                return redirect(url_for('thesis.create_thesis_form'))
+                return redirect(url_for("thesis.create_thesis_form"))
         else:
             flash("No deben haber campos vacios...")
-            return redirect(url_for('thesis.create_thesis_form'))
+            return redirect(url_for("thesis.create_thesis_form"))
     except Exception as ex:
         raise Exception(ex)
-    
+
+
 # Deactivate an thesis
-@thesis_bp.route('/desactivate_thesis/<int:id>')
+@thesis_bp.route("/desactivate_thesis/<int:id>")
 @login_required
 def desactivate_thesis(id):
     try:
         thesis = ControllerThesis.get_thesis_by_id(db, id)
         if thesis:
-            file_path = os.path.join(UPLOAD_FOLDER, thesis['pdf_link'])
+            file_path = os.path.join(UPLOAD_FOLDER, thesis["pdf_link"])
             try:
                 if os.path.exists(file_path):
                     os.remove(file_path)
@@ -98,13 +110,14 @@ def desactivate_thesis(id):
 
             ControllerThesis.desactivate_thesis(db, id)
             flash("Tesis Eliminado Exitosamente...")
-            return redirect(url_for('thesis.myThesis'))
+            return redirect(url_for("thesis.myThesis"))
         else:
             flash("No se pudo eliminar la Tesis...")
-            return redirect(url_for('thesis.myThesis'))
+            return redirect(url_for("thesis.myThesis"))
     except Exception as ex:
         raise Exception(ex)
 
+
 # Define a function to check if the file extension is allowed
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() == 'pdf'
+    return "." in filename and filename.rsplit(".", 1)[1].lower() == "pdf"
--- src/controllers/ControllerThesis.py
+++ src/controllers/ControllerThesis.py
@@ -2,9 +2,9 @@ from flask_login import current_user
 from models.Thesis import Thesis
 from sqlalchemy import text
 
-class ControllerThesis():
 
-    #Method to get thesis by author_id
+class ControllerThesis:
+    # Method to get thesis by author_id
     @classmethod
     def getThesis(cls, db):
         try:
@@ -22,26 +22,39 @@ class ControllerThesis():
             )
             result = session.execute(sql, {"user_id": user_id})
             session.commit()
-            rows=result.fetchall()
+            rows = result.fetchall()
             thesiss = []
             if rows != None:
                 for row in rows:
-                    thesis = Thesis(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11])
+                    thesis = Thesis(
+                        row[0],
+                        row[1],
+                        row[2],
+                        row[3],
+                        row[4],
+                        row[5],
+                        row[6],
+                        row[7],
+                        row[8],
+                        row[9],
+                        row[10],
+                        row[11],
+                    )
                     thesiss.append(thesis)
                 return thesiss
             else:
                 return None
         except Exception as ex:
             raise Exception(ex)
-    
-    #Method to get thesis by thesis_id
+
+    # Method to get thesis by thesis_id
     @classmethod
     def get_thesis_by_id(cls, db, id):
         try:
             session = db.session()
             sql = text(
-                "SELECT T.thesis_id, T.title, T.abstract, T.submission_date, R.rating, T.pdf_link, T.thesis_status_id, A.author_id, P.firstname, P.lastname "
-                "FROM THESIS T " 
+                "SELECT T.thesis_id, T.title, T.abstract, T.submission_date, T.expiration_date, T.last_update_date, R.rating, T.pdf_link, T.thesis_status_id, A.author_id, P.firstname, P.lastname "
+                "FROM THESIS T "
                 "INNER JOIN AUTHOR_THESIS AT ON AT.thesis_id = T.thesis_id "
                 "INNER JOIN AUTHOR A ON A.author_id = AT.author_id "
                 "INNER JOIN PERSON P ON P.person_id = A.person_id "
@@ -54,50 +67,55 @@ class ControllerThesis():
             row = result.fetchone()
             if row:
                 thesis = {
-                    'thesis_id': row[0],
-                    'title': row[1],
-                    'abstract' : row[2],
-                    'submission_date' : row[3],
-                    'rating' : row[4],
-                    'pdf_link' : row[5],
-                    'thesis_status_id': row[6],
-                    'author_id': row[7],
-                    'firstname': row[8],
-                    'lastname': row[9],
+                    "thesis_id": row[0],
+                    "title": row[1],
+                    "abstract": row[2],
+                    "submission_date": row[3],
+                    "expiration_date": row[4],
+                    "last_update_date": row[5],
+                    "rating": row[6],
+                    "pdf_link": row[7],
+                    "thesis_status_id": row[8],
+                    "author_id": row[9],
+                    "firstname": row[10],
+                    "lastname": row[11],
                 }
                 return thesis
             else:
                 return None
         except Exception as ex:
             raise Exception(ex)
-        
-    #Method to save thesis
+
+    # Method to save thesis project
     @classmethod
-    def createThesis(cls, db, title, abstract, pdf_link):
+    def createProjectThesis(cls, db, title, abstract, pdf_link):
         try:
             person_id = current_user.person_id
             session = db.session()
             sql = text(
-                "INSERT INTO THESIS (title, abstract, submission_date, pdf_link, thesis_status_id) "
-                "VALUES (:title, :abstract, CURDATE(), :pdf_link, 1); "
+                "CALL InsertThesisProject(:title, :abstract, :pdf_link, :person_id)"
+            )
+            sql = text(
+                "INSERT INTO THESIS (title, abstract, submission_date, expiration_date, last_update_date, pdf_link, thesis_status_id, is_project) "
+                "VALUES (:title, :abstract, CURDATE(), DATE_ADD(CURDATE(), INTERVAL 2 YEAR), CURDATE(),:pdf_link, 1, 1); "
                 "SET @thesis_id = LAST_INSERT_ID(); "
                 "SET @author_id = (SELECT author_id FROM author where person_id = :person_id); "
                 "INSERT INTO AUTHOR_THESIS (author_id, thesis_id) "
                 "VALUES (@author_id, @thesis_id);"
             )
             params = {
-                'title': title,
-                'abstract': abstract,
-                'pdf_link': pdf_link,
-                'person_id': person_id
+                "title": title,
+                "abstract": abstract,
+                "pdf_link": pdf_link,
+                "person_id": person_id,
             }
             session.execute(sql, params)
             session.commit()
-            return {'message': 'Thesis created successfully'}, 201
+            return {"message": "Thesis project created successfully"}, 201
         except Exception as ex:
             raise Exception(ex)
-    
-    #Method to update thesis
+
+    # Method to update thesis
     @classmethod
     def updateThesis(cls, db, id, title, abstract):
         try:
@@ -107,29 +125,21 @@ class ControllerThesis():
                 "SET title = :title, abstract = :abstract "
                 "WHERE  thesis_id = :id; "
             )
-            params = {
-                'id': id,
-                'title': title,
-                'abstract': abstract
-            }
+            params = {"id": id, "title": title, "abstract": abstract}
             session.execute(sql, params)
             session.commit()
-            return {'message': 'Thesis updated successfully'}, 200
+            return {"message": "Thesis updated successfully"}, 200
         except Exception as ex:
             raise Exception(ex)
-        
-    #Logical deletion of an thesis
-    @classmethod    
+
+    # Logical deletion of an thesis
+    @classmethod
     def desactivate_thesis(cls, db, id):
         try:
             session = db.session()
-            sql = text(
-                "UPDATE THESIS "
-                "SET is_deleted = 1 "
-                "WHERE thesis_id = :id"
-            )
+            sql = text("UPDATE THESIS " "SET is_deleted = 1 " "WHERE thesis_id = :id")
             session.execute(sql, {"id": id})
             session.commit()
-            return {'message': 'Thesis created successfully'}, 200
+            return {"message": "Thesis created successfully"}, 200
         except Exception as ex:
             raise Exception(ex)
